d89edf2e74a5b04e8a505fdd20611f5abcd28cef
Mister Tanatswa <tanatswasingano@gmail.com>
2022-05-15 21:40:11 -0400

    city weather retrieval

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+function domHtml() {
+  const cityEl = document.getElementById("enter-city");
+  const searchEl = document.getElementById("search-button");
+  const clearEl = document.getElementById("clear-history");
+  const nameEl = document.getElementById("city-name");
+  const currentPicEl = document.getElementById("current-pic");
+  const currentTempEl = document.getElementById("temperature");
+  const currentHumidityEl = document.getElementById("humidity");
+  const currentWindEl = document.getElementById("wind-speed");
+  const currentUVEl = document.getElementById("UV-index");
+  const historyEl = document.getElementById("history");
+  var fivedayEl = document.getElementById("fiveday-header");
+  var todayweatherEl = document.getElementById("today-weather");
+  let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+  const APIKey = "65873880d87d81bde96b44501ed7e0bd";
+}
